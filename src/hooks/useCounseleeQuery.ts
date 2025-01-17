898f572dc74cda04e33155feca8287597cc37d23
ohoCoding <youngho7482@gmail.com>
2025-01-17 21:01:40 +0900

    feat: :sparkles: react-query 활용 내담자 기본 정보 조회 API 설정

---
+++ src/hooks/useCounseleeQuery.ts
@@ -0,0 +1,20 @@
+import { CounseleeControllerApi } from '@/api/api';
+import { useQuery } from '@tanstack/react-query';
+
+const counseleeControllerApi = new CounseleeControllerApi();
+//내담자 기본 정보 조회
+const selectBaseInformation = async (counselSessionId: string) => {
+  // 내담자 기본 정보 조회 API 호출
+  const response = await counseleeControllerApi.selectCounseleeBaseInformation(
+    counselSessionId,
+  );
+  return response.data.data;
+};
+
+//실제 사용하는 커스텀 훅
+export const useSelectCounseleeInfo = (counselSessionId: string) =>
+  useQuery({
+    queryKey: ['dataItems', counselSessionId],
+    queryFn: () => selectBaseInformation(counselSessionId),
+    enabled: !!counselSessionId,
+  });
