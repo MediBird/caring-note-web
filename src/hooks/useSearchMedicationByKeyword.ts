dee4e96e93b6c05822001b569aca555b8960279c
문혜성 <moonhs97@gmail.com>
2025-01-18 23:20:06 +0900

    feat: :sparkles: 의약품 검색 api react-query 커스텀훅 추가

---
+++ src/hooks/useSearchMedicationByKeyword.ts
@@ -0,0 +1,20 @@
+import { MedicationControllerApi } from '@/api/api';
+import { useQuery } from '@tanstack/react-query';
+
+const medicationApi = new MedicationControllerApi();
+
+const searchMedicationByKeyword = async (keyword: string) => {
+  const response = await medicationApi.searchMedicationByKeyword(keyword);
+
+  return response.data.data ?? [];
+};
+
+export const useSearchMedicationByKeyword = (keyword: string) => {
+  const { data, isSuccess } = useQuery({
+    queryKey: ['searchMedicationByKeyword', keyword],
+    queryFn: () => searchMedicationByKeyword(keyword),
+    enabled: keyword.length > 1,
+  });
+
+  return { data, isSuccess };
+};
