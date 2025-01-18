dc97de85862c7d4565c693cd1524fb2b3eccf124
ohoCoding <youngho7482@gmail.com>
2025-01-18 22:18:09 +0900

    refactor: :recycle: input 높이 수정

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,14 +1,14 @@
-import * as React from "react";
+import * as React from 'react';
 
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
         type={type}
         className={cn(
-          "flex h-9 w-full rounded-md border-grayscale-30 bg-white px-3 py-2 text-base font-medium border-2",
+          'flex h-10 w-full rounded-md border-grayscale-30 bg-white px-3 py-2 text-base font-medium border-2',
           className,
         )}
         ref={ref}
@@ -17,6 +17,6 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
     );
   },
 );
-Input.displayName = "Input";
+Input.displayName = 'Input';
 
 export { Input };
