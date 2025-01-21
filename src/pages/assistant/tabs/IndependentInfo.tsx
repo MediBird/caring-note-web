81fc898cd4d47c0fe6167f731e9963faa09429b7
ohoCoding <youngho7482@gmail.com>
2025-01-21 20:08:28 +0900

    refactor: :recycle: Input, Button 모듈화

--- src/pages/assistant/tabs/IndependentInfo.tsx
+++ src/pages/assistant/tabs/IndependentInfo.tsx
@@ -16,23 +16,28 @@ import {
   communicationTypes,
   usingKoreanTypes,
 } from '@/pages/assistant/constants/IndependentInfo';
-import {
-  CommunicationDTO,
-  EvacuationDTO,
-  IndependentLifeInformationDTO,
-  WalkingDTO,
-} from '@/api';
+import { IndependentLifeInformationDTO } from '@/api';
 import { useParams } from 'react-router-dom';
 import { useSelectCounselCard } from '@/hooks/useCounselAssistantQuery';
+
+type SectionFields = Record<string, string | undefined>;
+type ButtonOption = {
+  label: string;
+  value: string | number | boolean;
+};
 
 const IndependentInfo = () => {
   // 새로고침이 되었을 때도 active tab 을 잃지 않도록 컴포넌트 load 시 dispatch
   const dispatch = useAppDispatch();
-  const { counselSessionId } = useParams(); //useParams()를 통해 counselSessionId를 가져옴
+  //useParams()를 통해 counselSessionId를 가져옴
+  const { counselSessionId } = useParams();
+  //counselAssistantStore에서 counselAssistant와 setCounselAssistant를 가져옴
   const { counselAssistant, setCounselAssistant } = useCounselAssistantStore();
+
   useEffect(() => {
     dispatch(changeActiveTab('/assistant/view/livingInfo')); // 해당 tab의 url
   }, [dispatch]);
+
   // 상담 카드 조회
   const { data: selectCounselCardAssistantInfo } = useSelectCounselCard(
     counselSessionId ?? '',
@@ -44,58 +49,6 @@ const IndependentInfo = () => {
       communication: {},
     },
   );
-  const handleOptionChange = (
-    option: string,
-    section: keyof IndependentLifeInformationDTO,
-    field: keyof WalkingDTO | keyof EvacuationDTO | keyof CommunicationDTO,
-  ) => {
-    setFormData((prev) => {
-      let current: string[] = [];
-      if (
-        section === 'walking' &&
-        (field === 'walkingMethods' ||
-          field === 'walkingEquipments' ||
-          field === 'etcNote')
-      ) {
-        current = (prev.walking?.[field] ?? []) as string[];
-      } else if (
-        section === 'evacuation' &&
-        (field === 'evacuationMethods' || field === 'etcNote')
-      ) {
-        current = (prev.evacuation?.[field] ?? []) as string[];
-      } else if (
-        section === 'communication' &&
-        (field === 'sights' ||
-          field === 'hearings' ||
-          field === 'communications' ||
-          field === 'usingKoreans')
-      ) {
-        current = (prev.communication?.[field] ?? []) as string[];
-      }
-      const updated = current.includes(option)
-        ? current.filter((item) => item !== option)
-        : [...current, option];
-      return {
-        ...prev,
-        [section]: {
-          ...(typeof prev[section] === 'object' ? prev[section] : {}),
-          [field]: updated,
-        },
-      };
-    });
-  };
-  const handleInputChange = (
-    section: 'walking' | 'evacuation' | 'communication',
-    e: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    setFormData((prev) => ({
-      ...prev,
-      [section]: {
-        ...prev[section],
-        [e.target.name]: e.target.value,
-      },
-    }));
-  };
 
   useEffect(() => {
     if (selectCounselCardAssistantInfo?.independentLifeInformation) {
@@ -115,6 +68,109 @@ const IndependentInfo = () => {
       });
     }
   }, [formData, setCounselAssistant, counselAssistant]);
+  const handleInputChange = (
+    section: keyof IndependentLifeInformationDTO,
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setFormData((prev) => ({
+      ...prev,
+      [section]: {
+        ...(prev[section] as SectionFields),
+        [e.target.name]: e.target.value,
+      },
+    }));
+  };
+
+  // 배열 값 토글 함수
+  const toggleArrayValue = (
+    section: keyof IndependentLifeInformationDTO,
+    field: string,
+    value: string,
+  ) => {
+    setFormData((prev) => {
+      const currentValues =
+        (prev[section] as SectionFields)?.[field] || ([] as string[]);
+      const updatedValues = currentValues.includes(value)
+        ? Array.isArray(currentValues)
+          ? currentValues.filter((v) => v !== value)
+          : []
+        : [...currentValues, value];
+      return {
+        ...prev,
+        [section]: {
+          ...(prev[section] as SectionFields),
+          [field]: updatedValues,
+          ...(value === '기타' &&
+            !updatedValues.includes('기타') && { etcNote: '' }), // '기타' 해제 시 etcNote 초기화
+        },
+      };
+    });
+  };
+
+  const renderMultiSelectButtons = (
+    label: string,
+    options: ButtonOption[],
+    section: keyof IndependentLifeInformationDTO,
+    field: string,
+    extraFieldName?: string, // '기타' 입력 필드 이름
+    extraFieldPlaceholder?: string, // '기타' 입력 필드 placeholder
+  ) => {
+    const isEtcSelected =
+      extraFieldName &&
+      typeof formData[section] === 'object' &&
+      (formData[section] as SectionFields)?.[field]?.includes('기타'); // '기타' 선택 여부 확인
+
+    return (
+      <div className="p-4">
+        <Label htmlFor={field} className="font-bold">
+          {label}
+        </Label>
+        <p className="mt-3 mb-3 text-sm text-gray-500">
+          여러개를 동시에 선택할 수 있어요.
+        </p>
+        <div className="gap-2">
+          {options.map((option) => (
+            <Button
+              key={option.label}
+              type="button"
+              variant={
+                (formData[section] as SectionFields)?.[field]?.includes(
+                  option.value.toString(),
+                )
+                  ? 'pressed'
+                  : 'nonpressed'
+              }
+              className="pl-3 pr-3 mt-3 mr-3 font-medium rounded-lg"
+              size="lg"
+              onClick={() =>
+                toggleArrayValue(section, field, option.value.toString())
+              }>
+              {option.label}
+            </Button>
+          ))}
+        </div>
+        {/* '기타' 선택 시 입력 필드 표시 */}
+        {isEtcSelected && extraFieldName && (
+          <div className="pt-4 mb-6">
+            <Label htmlFor={extraFieldName} className="font-bold">
+              기타
+            </Label>
+            <Input
+              id={extraFieldName}
+              name={extraFieldName}
+              placeholder={extraFieldPlaceholder}
+              value={
+                (formData[section] as SectionFields)?.[extraFieldName] || ''
+              }
+              onChange={(e) => handleInputChange(section, e)}
+              className="mt-3"
+            />
+          </div>
+        )}
+      </div>
+    );
+  };
+
   return (
     <>
       <TabContentContainer>
@@ -122,80 +178,21 @@ const IndependentInfo = () => {
         <div className="flex items-start justify-between space-x-4">
           <CardContainer title={'보행'} variant="error" itemName="baseInfo">
             {/* 보행 여부 */}
-            <div className="inline-block p-4">
-              <Label htmlFor="walking" className="font-bold">
-                보행여부
-              </Label>
-              <p className="mt-3 mb-3 text-sm text-gray-500">
-                여러개를 동시에 선택할 수 있어요.
-              </p>
-              <div className="flex gap-2">
-                {IswalkingTypes.map((walking) => (
-                  <Button
-                    key={walking}
-                    id="walking"
-                    type="button"
-                    variant={
-                      formData.walking?.walkingMethods?.includes(walking)
-                        ? 'pressed'
-                        : 'nonpressed'
-                    }
-                    className="pl-2 pr-2 font-medium rounded-lg"
-                    size="lg"
-                    onClick={() =>
-                      handleOptionChange(walking, 'walking', 'walkingMethods')
-                    }>
-                    {walking}
-                  </Button>
-                ))}
-              </div>
-            </div>
+            {renderMultiSelectButtons(
+              '보행 여부',
+              IswalkingTypes,
+              'walking',
+              'walkingMethods',
+            )}
 
             {/* 이동 장비 */}
-            <div className="p-4">
-              <Label htmlFor="tools" className="font-bold">
-                이동장비
-              </Label>
-              <p className="mt-3 mb-3 text-sm text-gray-500">
-                여러개를 동시에 선택할 수 있어요.
-              </p>
-              <div className="flex gap-2">
-                {walkingTools.map((tool) => (
-                  <Button
-                    key={tool}
-                    id="tools"
-                    type="button"
-                    variant={
-                      formData.walking?.walkingEquipments?.includes(tool)
-                        ? 'pressed'
-                        : 'nonpressed'
-                    }
-                    className="pl-2 pr-2 font-medium rounded-lg"
-                    size="lg"
-                    onClick={() =>
-                      handleOptionChange(tool, 'walking', 'walkingEquipments')
-                    }>
-                    {tool}
-                  </Button>
-                ))}
-              </div>
-            </div>
-
-            {/* 기타 */}
-            {formData.walking?.walkingEquipments?.includes('기타') && (
-              <div className="p-4 mb-6">
-                <Label htmlFor="etcNote" className="font-bold">
-                  기타
-                </Label>
-                <Input
-                  id="etcNote"
-                  name="etcNote"
-                  placeholder="‘기타' 선택시, 이동 장비를 작성해주세요."
-                  value={formData.walking?.etcNote || ''}
-                  onChange={(e) => handleInputChange('walking', e)}
-                  className="mt-3 "
-                />
-              </div>
+            {renderMultiSelectButtons(
+              '이동 장비',
+              walkingTools,
+              'walking',
+              'walkingEquipments',
+              'etcNote',
+              '기타 선택시, 이동 장비를 작성해주세요.',
             )}
           </CardContainer>
         </div>
@@ -204,57 +201,13 @@ const IndependentInfo = () => {
         <div className="flex items-start justify-between space-x-4">
           <CardContainer title={'배변'} itemName="baseInfo">
             {/* 배변 처리 방식 */}
-            <div className="w-full p-4">
-              <Label htmlFor="evacuationMethods" className="font-bold">
-                배변 처리 방식
-              </Label>
-              <p className="mt-3 mb-3 text-sm text-gray-500">
-                여러개를 동시에 선택할 수 있어요.
-              </p>
-
-              <div className="flex gap-2">
-                {evacuationMethods.map((evacuation) => (
-                  <Button
-                    key={evacuation}
-                    id="evacuation"
-                    type="button"
-                    variant={
-                      formData.evacuation?.evacuationMethods?.includes(
-                        evacuation,
-                      )
-                        ? 'pressed'
-                        : 'nonpressed'
-                    }
-                    className="pl-2 pr-2 font-medium rounded-lg"
-                    size="lg"
-                    onClick={() =>
-                      handleOptionChange(
-                        evacuation,
-                        'evacuation',
-                        'evacuationMethods',
-                      )
-                    }>
-                    {evacuation}
-                  </Button>
-                ))}
-              </div>
-            </div>
-
-            {/* 기타 */}
-            {formData.evacuation?.evacuationMethods?.includes('기타') && (
-              <div className="p-4 mb-6">
-                <Label htmlFor="etcNote" className="font-bold">
-                  기타
-                </Label>
-                <Input
-                  id="etcNote"
-                  name="etcNote"
-                  placeholder="‘기타' 선택시, 배변 처리 방식을 작성해주세요."
-                  value={formData.evacuation?.etcNote || ''}
-                  onChange={(e) => handleInputChange('evacuation', e)}
-                  className="mt-3 "
-                />
-              </div>
+            {renderMultiSelectButtons(
+              '배변 처리 방식',
+              evacuationMethods,
+              'evacuation',
+              'evacuationMethods',
+              'etcNote',
+              '기타 선택시, 배변 처리 방식을 작성해주세요.',
             )}
           </CardContainer>
         </div>
@@ -263,134 +216,36 @@ const IndependentInfo = () => {
         <div className="flex items-start justify-between space-x-4">
           <CardContainer title={'의사소통 정도'} itemName="baseInfo">
             {/* 시력 */}
-            <div className="w-full p-4">
-              <Label htmlFor="sight" className="font-bold">
-                시력
-              </Label>
-              <p className="mt-3 mb-3 text-sm text-gray-500">
-                여러개를 동시에 선택할 수 있어요.
-              </p>
-              <div className="flex gap-2">
-                {sightTypes.map((sight) => (
-                  <Button
-                    key={sight}
-                    id="sight"
-                    type="button"
-                    variant={
-                      formData.communication?.sights?.includes(sight)
-                        ? 'pressed'
-                        : 'nonpressed'
-                    }
-                    className="pl-2 pr-2 font-medium rounded-lg"
-                    size="lg"
-                    onClick={() =>
-                      handleOptionChange(sight, 'communication', 'sights')
-                    }>
-                    {sight}
-                  </Button>
-                ))}
-              </div>
-            </div>
+            {renderMultiSelectButtons(
+              '시력',
+              sightTypes,
+              'communication',
+              'sights',
+            )}
 
             {/* 청력 */}
-            <div className="w-full p-4">
-              <Label htmlFor="hearing" className="font-bold">
-                청력
-              </Label>
-              <p className="mt-3 mb-3 text-sm text-gray-500">
-                여러개를 동시에 선택할 수 있어요.
-              </p>
-              <div className="flex gap-2">
-                {hearingTypes.map((hearing) => (
-                  <Button
-                    key={hearing}
-                    id="hearing"
-                    type="button"
-                    variant={
-                      formData.communication?.hearings?.includes(hearing)
-                        ? 'pressed'
-                        : 'nonpressed'
-                    }
-                    className="pl-2 pr-2 font-medium rounded-lg"
-                    size="lg"
-                    onClick={() =>
-                      handleOptionChange(hearing, 'communication', 'hearings')
-                    }>
-                    {hearing}
-                  </Button>
-                ))}
-              </div>
-            </div>
+            {renderMultiSelectButtons(
+              '청력',
+              hearingTypes,
+              'communication',
+              'hearings',
+            )}
 
             {/* 언어 소통 */}
-            <div className="w-full p-4">
-              <Label htmlFor="communication" className="font-bold">
-                언어 소통
-              </Label>
-              <p className="mt-3 mb-3 text-sm text-gray-500">
-                여러개를 동시에 선택할 수 있어요.
-              </p>
-              <div className="flex gap-2">
-                {communicationTypes.map((communication) => (
-                  <Button
-                    key={communication}
-                    id="communication"
-                    type="button"
-                    variant={
-                      formData.communication?.communications?.includes(
-                        communication,
-                      )
-                        ? 'pressed'
-                        : 'nonpressed'
-                    }
-                    className="pl-2 pr-2 font-medium rounded-lg"
-                    size="lg"
-                    onClick={() =>
-                      handleOptionChange(
-                        communication,
-                        'communication',
-                        'communications',
-                      )
-                    }>
-                    {communication}
-                  </Button>
-                ))}
-              </div>
-            </div>
+            {renderMultiSelectButtons(
+              '언어 소통',
+              communicationTypes,
+              'communication',
+              'communications',
+            )}
 
             {/* 한글 사용 */}
-            <div className="w-full p-4">
-              <Label htmlFor="useKorean" className="font-bold">
-                한글 사용
-              </Label>
-              <p className="mt-3 mb-3 text-sm text-gray-500">
-                여러개를 동시에 선택할 수 있어요.
-              </p>
-              <div className="flex gap-2">
-                {usingKoreanTypes.map((useKorean) => (
-                  <Button
-                    key={useKorean}
-                    id="useKorean"
-                    type="button"
-                    variant={
-                      formData.communication?.usingKoreans?.includes(useKorean)
-                        ? 'pressed'
-                        : 'nonpressed'
-                    }
-                    className="pl-2 pr-2 font-medium rounded-lg"
-                    size="lg"
-                    onClick={() =>
-                      handleOptionChange(
-                        useKorean,
-                        'communication',
-                        'usingKoreans',
-                      )
-                    }>
-                    {useKorean}
-                  </Button>
-                ))}
-              </div>
-            </div>
+            {renderMultiSelectButtons(
+              '한글 사용',
+              usingKoreanTypes,
+              'communication',
+              'usingKoreans',
+            )}
           </CardContainer>
         </div>
       </TabContentContainer>
