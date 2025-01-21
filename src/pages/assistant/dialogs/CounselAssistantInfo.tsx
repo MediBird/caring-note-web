378db2fce2c7f8bd966676a0a49145acd0ea5a6a
ohoCoding <youngho7482@gmail.com>
2025-01-21 20:09:44 +0900

    refactor: :recycle: Dialog 타입 EXIT 추가에 따른 중간에 나가기 추가

--- src/pages/assistant/dialogs/CounselAssistantInfo.tsx
+++ src/pages/assistant/dialogs/CounselAssistantInfo.tsx
@@ -31,15 +31,20 @@ const SaveCounselAssistant = ({
   onClose: () => void;
 }) => {
   const navigate = useNavigate();
+  // useParms() 를 통한 URL 파라미터 값 추출
   const { counselSessionId } = useParams();
+  // useDetailCounselSessionStore()를 통해 counselSessionStore의 상태값 추출
   const detail = useDetailCounselSessionStore((state) => state.detail);
+  // useCounselAssistantStore()를 통해 counselAssistantStore의 상태값 추출
   const { counselAssistant, setCounselAssistant } = useCounselAssistantStore();
+  // useUpdateCounselAssistant()를 통해 updateCounselAssistant 함수 추출
   const updateCounselAssistant = useUpdateCounselAssistant();
+  // usePostCounselAssistant()를 통해 addCounselCard 함수 추출
   const addCounselCard = usePostCounselAssistant();
 
   // 상담카드 저장 및 임시저장
   const handleSaveCounselAssistant = () => {
-    const isTempSave = dialogType === 'TEMP_SAVE'; // 타입 확인
+    const isTempSave = dialogType === 'TEMP_SAVE' || dialogType === 'EXIT'; // 타입 확인
     const cardRecordStatus = isTempSave // 기록 상태 확인
       ? UpdateCounselCardReqCardRecordStatusEnum.Recording
       : AddCounselCardReqCardRecordStatusEnum.Recorded;
@@ -92,35 +97,56 @@ const SaveCounselAssistant = ({
       <DialogOverlay />
       <DialogContent className="w-[400px] h-[190px] flex flex-col justify-between">
         <DialogHeader className="justify-between pt-2">
-          <DialogTitle className="flex text-2xl font-bold text-grayscale-100">
+          <DialogTitle className="flex pt-1 text-xl font-bold text-grayscale-100">
             {dialogType === 'REGISTER' && '기록을 완료하시겠어요?'}
             {dialogType === 'TEMP_SAVE' && '임시 저장하시겠어요?'}
+            {dialogType === 'EXIT' && '기초 설문 작성이 완료되지 않았어요.'}
           </DialogTitle>
-          <img
-            src={CloseButton}
-            className="flex pb-5 cursor-pointer"
-            onClick={onClose}
-            alt="Close"
-          />
+          <div className="text-center">
+            <img
+              src={CloseButton}
+              className="flex pb-5 cursor-pointer"
+              onClick={onClose}
+              alt="Close"
+            />
+          </div>
         </DialogHeader>
 
         <div className="bg-white rounded-lg">
-          <p className="text-sm text-grayscale-50">
+          <p className="text-base text-grayscale-80">
             {dialogType === 'REGISTER' && '기초 상담 기록이 저장됩니다.'}
             {dialogType === 'TEMP_SAVE' &&
               '지금까지 작성하신 내용이 저장됩니다.'}
+            {dialogType === 'EXIT' && (
+              <>
+                여기서 나가시겠습니까?
+                <br />
+                나중에 이어서 작성을 완료할 수 있어요.
+              </>
+            )}
           </p>
         </div>
 
         <DialogFooter className="flex justify-end">
-          <Button className="ml-6" variant="secondary" onClick={onClose}>
-            취소
+          <Button
+            className="ml-6"
+            variant="secondary"
+            onClick={
+              dialogType === 'EXIT' ? handleSaveCounselAssistant : onClose
+            }>
+            {dialogType === 'EXIT' ? '임시저장' : '취소'}
           </Button>
           <Button
             className="ml-6"
             variant="primary"
-            onClick={handleSaveCounselAssistant}>
-            확인
+            onClick={() => {
+              if (dialogType === 'EXIT') {
+                navigate('/assistant');
+              } else {
+                handleSaveCounselAssistant();
+              }
+            }}>
+            {dialogType === 'EXIT' ? '나가기' : '확인'}
           </Button>
         </DialogFooter>
       </DialogContent>
