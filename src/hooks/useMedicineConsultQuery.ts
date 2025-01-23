a11cf9ab1735bdc86bc45b65ec02072a17f67be9
문혜성 <moonhs97@gmail.com>
2025-01-23 22:57:19 +0900

    refactor: :bug: 복약상담 highlight DTO 변경 대응

--- src/hooks/useMedicineConsultQuery.ts
+++ src/hooks/useMedicineConsultQuery.ts
@@ -1,62 +1,61 @@
-import { MedicationCounselControllerApi } from "@/api/api";
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { MedicineConsultDTO }  from "@/types/MedicineConsultDTO";
+import { MedicationCounselControllerApi } from '@/api/api';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { MedicineConsultDTO } from '@/types/MedicineConsultDTO';
 
 const medicationCounselControllerApi = new MedicationCounselControllerApi();
 
 const selectMedicationCounsel = async (counselSessionId: string) => {
+  const response = await medicationCounselControllerApi.selectMedicationCounsel(
+    counselSessionId,
+  );
 
-    const response = await medicationCounselControllerApi.selectMedicationCounsel(counselSessionId);
-  
-    return response.data.data||{
-        medicationCounselId: '',
-        counselRecord: '',
-        counselRecordHighlights: []
-      };
-
-}
-
-
-const saveMedicationCounsel = async (medicineConsultDTO: MedicineConsultDTO) => {
-
-    if (medicineConsultDTO.medicationCounselId === "") {
-    
-        await medicationCounselControllerApi.addMedicationCounsel({
-            counselSessionId: medicineConsultDTO.counselSessionId,
-            counselRecord: medicineConsultDTO.counselRecord,
-            counselRecordHighlights: medicineConsultDTO.counselRecordHighlights,
-          });
-    } else {
-
-        await medicationCounselControllerApi.updateMedicationCounsel({
-            medicationCounselId: medicineConsultDTO.medicationCounselId,
-            counselRecord: medicineConsultDTO.counselRecord,
-            counselRecordHighlights: medicineConsultDTO.counselRecordHighlights,
-          });
+  return (
+    response.data.data || {
+      medicationCounselId: '',
+      counselRecord: '',
+      counselRecordHighlights: [],
     }
+  );
+};
 
-    return medicineConsultDTO.counselSessionId;
-}
+const saveMedicationCounsel = async (
+  medicineConsultDTO: MedicineConsultDTO,
+) => {
+  if (medicineConsultDTO.medicationCounselId === '') {
+    await medicationCounselControllerApi.addMedicationCounsel({
+      counselSessionId: medicineConsultDTO.counselSessionId,
+      counselRecord: medicineConsultDTO.counselRecord,
+      counselRecordHighlights: medicineConsultDTO.counselRecordHighlights,
+    });
+  } else {
+    await medicationCounselControllerApi.updateMedicationCounsel({
+      medicationCounselId: medicineConsultDTO.medicationCounselId,
+      counselRecord: medicineConsultDTO.counselRecord,
+      counselRecordHighlights: medicineConsultDTO.counselRecordHighlights,
+    });
+  }
 
-export const useSelectMedicineConsult = (counselSessionId?: string) =>{
+  return medicineConsultDTO.counselSessionId;
+};
 
-    return useQuery({
-        queryKey: ["SelectMedicationConsult", counselSessionId],
-        queryFn: () => selectMedicationCounsel(counselSessionId!),
-        enabled: !!counselSessionId,
-      });
-    }
+export const useSelectMedicineConsult = (counselSessionId?: string) => {
+  return useQuery({
+    queryKey: ['SelectMedicationConsult', counselSessionId],
+    queryFn: () => selectMedicationCounsel(counselSessionId!),
+    enabled: !!counselSessionId,
+  });
+};
 
 export const useSaveMedicineConsult = () => {
+  const queryClient = useQueryClient();
 
-    const queryClient = useQueryClient();
-
-    return useMutation<string | undefined, Error, MedicineConsultDTO>({
-          mutationFn: (medicineConsultDTO: MedicineConsultDTO) =>
-            saveMedicationCounsel(medicineConsultDTO),
-          onSuccess: (counselSessionId) => {      
-            queryClient.invalidateQueries({
-                queryKey: ["SelectMedicationConsult", counselSessionId]});  
-        }
-        });
-    };
+  return useMutation<string | undefined, Error, MedicineConsultDTO>({
+    mutationFn: (medicineConsultDTO: MedicineConsultDTO) =>
+      saveMedicationCounsel(medicineConsultDTO),
+    onSuccess: (counselSessionId) => {
+      queryClient.invalidateQueries({
+        queryKey: ['SelectMedicationConsult', counselSessionId],
+      });
+    },
+  });
+};
--- src/types/MedicineConsultDTO.ts
+++ src/types/MedicineConsultDTO.ts
@@ -1,6 +1,12 @@
 export interface MedicineConsultDTO {
-    counselSessionId: string;
-    medicationCounselId: string;
-    counselRecord: string;
-    counselRecordHighlights: Array<string>;
-  }
+  counselSessionId: string;
+  medicationCounselId: string;
+  counselRecord: string;
+  counselRecordHighlights: CounselRecordHighlights[];
+}
+
+export interface CounselRecordHighlights {
+  startIndex: number;
+  endIndex: number;
+  highlight: string;
+}
