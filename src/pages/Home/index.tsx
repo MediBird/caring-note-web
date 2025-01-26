213a8d4346f5fd0cb864c8170cbb5823bac3b50e
Sanghoon <bshjaws1@gmail.com>
2025-01-26 17:48:44 +0900

    fix: :rotating_light: fix a lint error (any type)

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -114,7 +114,7 @@ function Home() {
 
 export default Home;
 
-const getCardColumns = (): GridColDef[] => {
+const getCardColumns: () => GridColDef[] = () => {
   const columns: GridColDef[] = [
     {
       field: 'scheduledTime',
