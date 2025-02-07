b52343fbd0466d279c0187e737ad6acbfcd7d141
JW Desktop <wlsdhks5495@naver.com>
2025-02-07 09:43:39 +0900

    refactor: :construction: refactor DialogDescription section

--- src/pages/Consult/components/RecordingDialog.tsx
+++ src/pages/Consult/components/RecordingDialog.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ui/button';
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogFooter,
@@ -19,6 +20,8 @@ function RecordingDialog() {
     setOpen(true);
   }, []);
 
+  // refactoring . . .
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const handleClickLater = () => {
     setOpen(false);
   };
@@ -36,24 +39,25 @@ function RecordingDialog() {
             <p className="text-h3 font-bold">상담 녹음을 시작하시겠어요?</p>
           </DialogTitle>
         </DialogHeader>
-        <DialogDescription className="flex flex-col items-center m-0">
+        <div className="flex justify-center">
           <div className="w-[240px] h-[240px] bg-grayscale-10 mb-6" />
-          <p className="text-body1 font-medium text-grayscale-100 text-center">
-            상담 녹음 시 모든 내용을 빠짐없이 기록 가능합니다. <br />
-            목소리를 정확하게 인식할 수 있도록 마이크를 가까이 두세요.
-          </p>
+        </div>
+        <DialogDescription className="m-0 text-body1 font-medium text-grayscale-100 text-center">
+          상담 녹음 시 모든 내용을 빠짐없이 기록 가능합니다. <br />
+          목소리를 정확하게 인식할 수 있도록 마이크를 가까이 두세요.
         </DialogDescription>
         <DialogFooter className="flex w-full items-center justify-center m-0 p-5">
-          {/* 놀랍게도 <DialogClose asChild> 버그로 인해 Button component 를 못가져옴 */}
-          {/* <DialogClose asChild> */}
-          <Button
-            variant="secondary"
-            size="xl"
-            className="flex-1"
-            onClick={handleClickLater}>
-            나중에 시작
-          </Button>
-          {/* </DialogClose> */}
+          {/* Button component 를 못가져옴 ??? */}
+          <DialogClose asChild>
+            <Button
+              variant="secondary"
+              size="xl"
+              className="flex-1 m-0 p-0"
+              // onClick={handleClickLater}>
+            >
+              나중에 시작
+            </Button>
+          </DialogClose>
           <Button
             variant="primary"
             size="xl"
