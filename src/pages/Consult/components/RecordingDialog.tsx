d2228b3ce21f059944cb79dc174c1ec328609952
JW Desktop <wlsdhks5495@naver.com>
2025-02-07 11:54:05 +0900

    fix: :bug: fix recording dialog bugs

--- src/pages/Consult/components/RecordingDialog.tsx
+++ src/pages/Consult/components/RecordingDialog.tsx
@@ -11,6 +11,8 @@ import {
 import { useRecording } from '@/hooks/useRecording';
 import { useEffect, useState } from 'react';
 
+import logoBlack from '@/assets/logoBlack.png';
+
 function RecordingDialog() {
   const { startRecording } = useRecording();
   const [open, setOpen] = useState(false);
@@ -39,22 +41,22 @@ function RecordingDialog() {
             <p className="text-h3 font-bold">상담 녹음을 시작하시겠어요?</p>
           </DialogTitle>
         </DialogHeader>
-        <div className="flex justify-center">
-          <div className="w-[240px] h-[240px] bg-grayscale-10 mb-6" />
-        </div>
-        <DialogDescription className="m-0 text-body1 font-medium text-grayscale-100 text-center">
-          상담 녹음 시 모든 내용을 빠짐없이 기록 가능합니다. <br />
-          목소리를 정확하게 인식할 수 있도록 마이크를 가까이 두세요.
+        <DialogDescription asChild className="flex flex-col items-center m-0">
+          <div>
+            <img
+              className="w-[240px] h-[240px] border-2 border-grayscale-50 mb-4"
+              src={logoBlack}
+              alt="test-image"
+            />
+            <p className="font-medium text-center text-body1 text-grayscale-100">
+              상담 녹음 시 모든 내용을 빠짐없이 기록 가능합니다. <br />
+              목소리를 정확하게 인식할 수 있도록 마이크를 가까이 두세요.
+            </p>
+          </div>
         </DialogDescription>
         <DialogFooter className="flex w-full items-center justify-center m-0 p-5">
-          {/* Button component 를 못가져옴 ??? */}
           <DialogClose asChild>
-            <Button
-              variant="secondary"
-              size="xl"
-              className="flex-1 m-0 p-0"
-              // onClick={handleClickLater}>
-            >
+            <Button variant="secondary" size="xl" className="flex-1 m-0 p-0">
               나중에 시작
             </Button>
           </DialogClose>
