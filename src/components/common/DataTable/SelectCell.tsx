5c7dd28f963785607ca44104b8882f925098f18d
Moonhyeseong <moonhs97@gmail.com>
2025-02-16 02:51:43 +0900

    refactor: :art: select cell placeholder 옵션 지정 수정

--- src/components/common/DataTable/SelectCell.tsx
+++ src/components/common/DataTable/SelectCell.tsx
@@ -5,6 +5,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
+import { useMemo } from 'react';
 
 interface SelectCellProps {
   initialValue: string;
@@ -19,21 +20,37 @@ function SelectCell({
   placeholder,
   onValueChange,
 }: SelectCellProps) {
+  const hasCustomInitialValue = useMemo(
+    () => initialValue && !options.some((opt) => opt.value === initialValue),
+    [initialValue, options],
+  );
+
   return (
     <div className="flex flex-col gap-1">
       <Select value={initialValue} onValueChange={onValueChange}>
-        <SelectTrigger className="cursor-pointer border-none text-md">
-          <SelectValue placeholder={placeholder} />
+        <SelectTrigger
+          className={`cursor-pointer border-none text-md bg-transparent data-[state=open]:shadow-cell-shadow focus:outline-none ${
+            initialValue === '' && 'text-grayscale-40'
+          }`}>
+          <SelectValue placeholder={placeholder} className="cursor-pointer" />
         </SelectTrigger>
         <SelectContent>
           {options.map((option) => (
             <SelectItem
               key={option.value}
               value={option.value}
-              className="text-md">
+              className="text-md cursor-pointer">
               {option.label}
             </SelectItem>
           ))}
+          {hasCustomInitialValue && (
+            <SelectItem
+              key={initialValue}
+              value={initialValue}
+              className="hidden">
+              {initialValue}
+            </SelectItem>
+          )}
         </SelectContent>
       </Select>
     </div>
