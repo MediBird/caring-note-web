1b990da91e56e0eb72283e9e3315fcc2952068d6
Moonhyeseong <moonhs97@gmail.com>
2025-02-16 03:19:56 +0900

    refactor:

--- src/pages/Consult/components/table/WasteMedicationTable/index.tsx
+++ src/pages/Consult/components/table/WasteMedicationTable/index.tsx
@@ -1,20 +1,24 @@
 import { DataTable } from '@/components/common/DataTable/DataTable';
-import { createColumns } from './wasteMedicationColumns';
+import AddTableRowButton from '@/components/common/DataTable/AddTableRowButton';
 import { useWasteMedicationListStore } from '@/pages/Consult/hooks/store/useWasteMedicationListStore';
 import { useEffect } from 'react';
-import { useDeleteMedicationList } from '@/pages/Consult/hooks/query/useDeleteMedicationList';
-import { useWasteMedicationList } from '@/pages/Consult/hooks/query/useWasteMedicationListQuery';
+import { useDeleteMedicationList } from '@/pages/Consult/hooks/query/wasteMedicineRecord/useDeleteWasteMedicationList';
+import { useWasteMedicationList } from '@/pages/Consult/hooks/query/wasteMedicineRecord/useWasteMedicationListQuery';
 import { WasteMedicationListDTO } from '@/types/WasteMedicationDTO';
-import PlusBlueIcon from '@/assets/icon/24/add.outlined.blue.svg?react';
+import { createColumns } from './wasteMedicationColumns';
 
 interface WasteMedicationTableProps {
   counselSessionId: string;
+  showTable: boolean;
+  isNone: boolean;
 }
 
 const WasteMedicationTable = ({
   counselSessionId,
+  showTable,
+  isNone,
 }: WasteMedicationTableProps) => {
-  const { deleteMedicationListMutation } = useDeleteMedicationList();
+  const { deleteWasteMedicationListMutation } = useDeleteMedicationList();
 
   //폐의약품 목록 조회
   const {
@@ -32,16 +36,6 @@ const WasteMedicationTable = ({
   //초기 로딩시 폐의약품 목록 저장
   useEffect(() => {
     if (isSuccessWasteMedicationList && wasteMedicationListData) {
-      console.log(
-        wasteMedicationListData.map((item) => ({
-          rowId: item.rowId,
-          medicationId: item.medicationId ?? '',
-          medicationName: item.medicationName ?? '',
-          unit: item.unit ?? 0,
-          disposalReason: item.disposalReason ?? '',
-        })),
-      );
-
       setWasteMedicationList(
         wasteMedicationListData.map((item) => ({
           id: item.rowId ?? '',
@@ -60,7 +54,11 @@ const WasteMedicationTable = ({
     counselSessionId,
   ]);
 
-  const handleUpdateCell = (id: string, field: string, value: string) => {
+  const handleUpdateCell = (
+    id: string,
+    field: string,
+    value: string | number,
+  ) => {
     const updatedItem: WasteMedicationListDTO | undefined =
       wasteMedicationList.find((item) => item.id === id);
 
@@ -89,57 +87,51 @@ const WasteMedicationTable = ({
     });
   };
 
-  const columns = createColumns({
-    onDelete: (id) => {
-      deleteMedicationListMutation.mutate({
+  const handleDelete = (id: string) => {
+    if (id.includes('temp')) {
+      setWasteMedicationList(
+        wasteMedicationList.filter((item) => !item.id.includes('temp')),
+      );
+    } else {
+      deleteWasteMedicationListMutation.mutate({
         counselSessionId: counselSessionId as string,
         wasteMedicationRecordIds: [id],
       });
-    },
+    }
+  };
+
+  const columns = createColumns({
+    onDelete: handleDelete,
     handleUpdateCell,
     handleSearchEnter,
   });
+
+  const isShowAddTableRowButton = !showTable || !isNone;
 
   return (
     <DataTable
       columns={columns}
       data={wasteMedicationList}
       footer={
-        <AddMedicationButton
-          onClickAddButton={() => {
-            setWasteMedicationList([
-              ...wasteMedicationList,
-              {
-                id: 'temp',
-                medicationId: 'temp',
-                medicationName: '',
-                unit: 0,
-                disposalReason: '',
-              },
-            ]);
-          }}
-        />
+        isShowAddTableRowButton ? (
+          <AddTableRowButton
+            onClickAddButton={() => {
+              setWasteMedicationList([
+                ...wasteMedicationList,
+                {
+                  id: 'temp' + Math.random().toString(36).substring(2, 15),
+                  medicationId: '',
+                  medicationName: '',
+                  unit: 0,
+                  disposalReason: '',
+                },
+              ]);
+            }}
+          />
+        ) : null
       }
     />
   );
 };
 
 export default WasteMedicationTable;
-
-const AddMedicationButton = ({
-  onClickAddButton,
-}: {
-  onClickAddButton: () => void;
-}) => {
-  return (
-    <div
-      className="flex items-center hover:cursor-pointer gap-[10px]"
-      onClick={onClickAddButton}>
-      <PlusBlueIcon
-        className="inline-block"
-        width={24}
-        height={24}></PlusBlueIcon>
-      <span className="text-body1 text-primary-50 ">추가하기</span>
-    </div>
-  );
-};
