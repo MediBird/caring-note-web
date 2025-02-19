0295198558f7b4c9b9fc83cdc9c1e97bb06e0ee8
JW Desktop <wlsdhks5495@naver.com>
2025-02-19 19:53:50 +0900

    style(STT): :lipstick: modify dialog image

--- src/pages/Consult/components/recording/RecordingDialog.tsx
+++ src/pages/Consult/components/recording/RecordingDialog.tsx
@@ -9,18 +9,13 @@ import {
   DialogTitle,
 } from '@/components/ui/dialog';
 import { useRecording } from '@/hooks/useRecording';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import recordingDialog from '@/assets/recordingDialog.png';
 
 function RecordingDialog() {
   const { startRecording } = useRecording();
-  const [open, setOpen] = useState(false);
-
-  useEffect(() => {
-    // TODO : 오늘 일정에 상담일정 있음 && 기존 등록된 녹음 없는 경우
-    setOpen(true);
-  }, []);
+  const [open, setOpen] = useState(true);
 
   const handleClickSaveRecording = () => {
     startRecording();
@@ -30,16 +25,23 @@ function RecordingDialog() {
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent className="w-[480px]">
-        <DialogHeader className="m-6">
+        <DialogHeader className="mt-4 items-center justify-center">
           <DialogTitle>
-            <p className="text-h3 font-bold">상담 녹음을 시작하시겠어요?</p>
+            <p className="text-h3 font-bold center">
+              상담 녹음을 시작하시겠어요?
+            </p>
           </DialogTitle>
         </DialogHeader>
         <DialogDescription asChild className="flex flex-col items-center m-0">
           <div>
-            <img className="mb-4" src={recordingDialog} alt="recordingDialog" />
+            <img
+              className="mb-4 w-[240px] h-[240px]"
+              src={recordingDialog}
+              alt="recordingDialog"
+            />
             <p className="font-medium text-center text-body1 text-grayscale-100">
-              상담 녹음 시 모든 내용을 빠짐없이 기록 가능합니다. <br />
+              상담 녹음 시 모든 내용을 빠짐없이 기록 가능합니다.
+              <br />
               목소리를 정확하게 인식할 수 있도록 마이크를 가까이 두세요.
             </p>
           </div>
