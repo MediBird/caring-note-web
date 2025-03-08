5f2785468fcace852cc6b05932d258d2ad8ea4ee
Moonhyeseong <73605822+Moonhyeseong@users.noreply.github.com>
2025-03-08 10:37:05 +0900

    refactor: Layout 중복 제거 및 LeftNavBar 반응형 구현 (#160)

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,14 +1,46 @@
 import NavigationLeft from '@/components/NavigationLeft';
-import { Outlet } from 'react-router-dom';
-
+import { Outlet, useLocation } from 'react-router-dom';
+import { SidebarProvider } from '@/components/ui/sidebar';
+import { useMemo } from 'react';
+import NavigationRight from '@/components/NavigationRight';
+import { cn } from '@/lib/utils';
 function Layout() {
+  const location = useLocation();
+
+  const isSidebarOpen = useMemo(() => {
+    const isSurvey = location.pathname.includes('/survey');
+
+    if (isSurvey) {
+      return false;
+    }
+
+    return true;
+  }, [location.pathname]);
+
+  const isRightSideBarActive = useMemo(() => {
+    const isConsult = location.pathname.includes('/consult');
+
+    if (isConsult) {
+      return true;
+    }
+
+    return false;
+  }, [location.pathname]);
+
   return (
-    <div className="flex justify-start w-full h-auto">
-      <NavigationLeft />
-      <div className=" w-full min-h-screen ">
-        <Outlet />
+    <SidebarProvider defaultOpen={isSidebarOpen}>
+      <div className="flex justify-start w-full h-auto">
+        <NavigationLeft />
+        <main
+          className={cn(
+            'flex-1 overflow-auto',
+            isRightSideBarActive ? 'pr-[50px]' : '',
+          )}>
+          <Outlet />
+        </main>
+        {isRightSideBarActive && <NavigationRight />}
       </div>
-    </div>
+    </SidebarProvider>
   );
 }
 
