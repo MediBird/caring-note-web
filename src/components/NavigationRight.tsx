5134c86a4a13733c16f2127f5764249ace614a64
박진완 <>
2025-03-15 11:49:22 +0900

    fix: :bug: 오른쪽 input component 수정

--- src/components/NavigationRight.tsx
+++ src/components/NavigationRight.tsx
@@ -20,7 +20,7 @@ const NavigationRight = () => {
   const defaultMenu = () => {
     return (
       <div
-        className={`flex flex-col items-center justify-start h-screen ${
+        className={`flex h-screen flex-col items-center justify-start ${
           isOpen ? 'w-20' : ''
         } py-8`}
         onClick={toggleMenu}>
@@ -32,26 +32,26 @@ const NavigationRight = () => {
   return (
     <div
       className={cn(
-        'fixed top-0 right-0 h-full z-50 bg-white transition-width duration-300 shadow-nav-right',
+        'transition-width fixed right-0 top-0 z-50 h-full bg-white shadow-nav-right duration-300',
         isOpen ? 'w-[380px]' : 'w-16 hover:bg-primary-5',
       )}>
       {!isOpen && defaultMenu()}
       {isOpen && (
         <div className="flex justify-start">
           <button
-            className="absolute left-[-1.2rem] top-1/2 transform -translate-y-1/2 bg-white p-2 rounded-full shadow-nav-right z-[-1]"
+            className="absolute left-[-1.2rem] top-1/2 z-[-1] -translate-y-1/2 transform rounded-full bg-white p-2 shadow-nav-right"
             onClick={toggleMenu}>
             {isOpen && <ChevronRight size={24} className="pr-2" />}
           </button>
-          <div className="bg-white w-full">
+          <div className="w-full bg-white">
             <div className="flex items-center justify-between border-b-2 border-grayscale-10 p-4 pt-8">
               <span className="text-subtitle2 font-bold text-grayscale-90">
                 상담기록
               </span>
               <CloseBlackIcon width={24} height={24} onClick={toggleMenu} />
             </div>
-            <div className="flex flex-col items-center justify-between w-full gap-4 px-2 py-4">
-              <HighlightInput className="w-full" />
+            <div className="flex w-full flex-col items-center justify-between gap-4 px-2 py-4">
+              <HighlightInput className="h-[450px] w-full" />
               <Recording className="w-full" />
             </div>
           </div>
