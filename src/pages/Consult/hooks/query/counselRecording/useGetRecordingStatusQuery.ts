c49873dca713ea8a591a159fda34a7bf84c7a026
JW Desktop <wlsdhks5495@naver.com>
2025-03-18 00:53:40 +0900

    feat: :recycle: 상태조회 주기 수정

--- src/pages/Consult/hooks/query/counselRecording/useGetRecordingStatusQuery.ts
+++ src/pages/Consult/hooks/query/counselRecording/useGetRecordingStatusQuery.ts
@@ -24,7 +24,7 @@ export const useGetRecordingStatusQuery = (
     refetchInterval:
       recordingStatus === RecordingStatus.STTLoading ||
       recordingStatus === RecordingStatus.AILoading
-        ? 1000
+        ? 3000
         : false,
   });
 
