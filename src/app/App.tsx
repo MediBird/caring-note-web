4ab0c1480c0b4416fd3637fa4e822c5af740abb4
Moonhyeseong <moonhs97@gmail.com>
2025-04-24 23:00:42 +0900

    feat: :art Keycloak Provider 로딩 컴포넌트 설정

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,13 +3,21 @@ import { ReactKeycloakProvider } from '@react-keycloak/web';
 import { Toaster } from 'sonner';
 import keycloak, { initOptions, onKeycloakEvent } from './keycloak';
 import Router from './Router';
+import Spinner from '@/components/common/Spinner';
+
+const LoadingScreen = () => (
+  <div className="flex h-screen w-screen items-center justify-center">
+    <Spinner />
+  </div>
+);
 
 const App = () => {
   return (
     <ReactKeycloakProvider
       authClient={keycloak}
       initOptions={initOptions}
-      onEvent={onKeycloakEvent}>
+      onEvent={onKeycloakEvent}
+      LoadingComponent={<LoadingScreen />}>
       <AuthContextProvider>
         <Router />
         <Toaster richColors position="bottom-right" />
