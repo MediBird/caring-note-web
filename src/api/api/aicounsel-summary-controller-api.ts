e779c848b107c14700c31e9353ef8cafd8cf316a
jawsbaek <bshjaws1@gmail.com>
2025-06-08 09:26:21 +0900

    feat: 새로운 상담 관련 모델 및 API 메서드 추가

--- src/api/api/aicounsel-summary-controller-api.ts
+++ src/api/api/aicounsel-summary-controller-api.ts
@@ -48,16 +48,54 @@ export const AICounselSummaryControllerApiAxiosParamCreator = function (configur
         /**
          * 
          * @summary convert Speech to Text
+         * @param {string} counselSessionId 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        convertSpeechToText: async (counselSessionId: string, options: RawAxiosRequestConfig = {}): Promise<RequestArgs> => {
+            // verify required parameter 'counselSessionId' is not null or undefined
+            assertParamExists('convertSpeechToText', 'counselSessionId', counselSessionId)
+            const localVarPath = `/v1/counsel/ai/{counselSessionId}/stt`
+                .replace(`{${"counselSessionId"}}`, encodeURIComponent(String(counselSessionId)));
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+
+            const localVarRequestOptions = { method: 'POST', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+            // authentication bearer-jwt required
+            // http bearer authentication required
+            await setBearerAuthToObject(localVarHeaderParameter, configuration)
+
+
+    
+            setSearchParams(localVarUrlObj, localVarQueryParameter);
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+
+            return {
+                url: toPathString(localVarUrlObj),
+                options: localVarRequestOptions,
+            };
+        },
+        /**
+         * 
+         * @summary convert Speech to Text
          * @param {File} audio 
          * @param {ConvertSpeechToTextReq} body 
          * @param {*} [options] Override http request option.
          * @throws {RequiredError}
          */
-        convertSpeechToText: async (audio: File, body: ConvertSpeechToTextReq, options: RawAxiosRequestConfig = {}): Promise<RequestArgs> => {
+        convertSpeechToText1: async (audio: File, body: ConvertSpeechToTextReq, options: RawAxiosRequestConfig = {}): Promise<RequestArgs> => {
             // verify required parameter 'audio' is not null or undefined
-            assertParamExists('convertSpeechToText', 'audio', audio)
+            assertParamExists('convertSpeechToText1', 'audio', audio)
             // verify required parameter 'body' is not null or undefined
-            assertParamExists('convertSpeechToText', 'body', body)
+            assertParamExists('convertSpeechToText1', 'body', body)
             const localVarPath = `/v1/counsel/ai/stt`;
             // use dummy base URL string because the URL constructor only accepts absolute URLs.
             const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
@@ -309,15 +347,28 @@ export const AICounselSummaryControllerApiFp = function(configuration?: Configur
         /**
          * 
          * @summary convert Speech to Text
+         * @param {string} counselSessionId 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async convertSpeechToText(counselSessionId: string, options?: RawAxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<SuccessRes>> {
+            const localVarAxiosArgs = await localVarAxiosParamCreator.convertSpeechToText(counselSessionId, options);
+            const localVarOperationServerIndex = configuration?.serverIndex ?? 0;
+            const localVarOperationServerBasePath = operationServerMap['AICounselSummaryControllerApi.convertSpeechToText']?.[localVarOperationServerIndex]?.url;
+            return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, localVarOperationServerBasePath || basePath);
+        },
+        /**
+         * 
+         * @summary convert Speech to Text
          * @param {File} audio 
          * @param {ConvertSpeechToTextReq} body 
          * @param {*} [options] Override http request option.
          * @throws {RequiredError}
          */
-        async convertSpeechToText(audio: File, body: ConvertSpeechToTextReq, options?: RawAxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<SuccessRes>> {
-            const localVarAxiosArgs = await localVarAxiosParamCreator.convertSpeechToText(audio, body, options);
+        async convertSpeechToText1(audio: File, body: ConvertSpeechToTextReq, options?: RawAxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<SuccessRes>> {
+            const localVarAxiosArgs = await localVarAxiosParamCreator.convertSpeechToText1(audio, body, options);
             const localVarOperationServerIndex = configuration?.serverIndex ?? 0;
-            const localVarOperationServerBasePath = operationServerMap['AICounselSummaryControllerApi.convertSpeechToText']?.[localVarOperationServerIndex]?.url;
+            const localVarOperationServerBasePath = operationServerMap['AICounselSummaryControllerApi.convertSpeechToText1']?.[localVarOperationServerIndex]?.url;
             return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, localVarOperationServerBasePath || basePath);
         },
         /**
@@ -399,13 +450,23 @@ export const AICounselSummaryControllerApiFactory = function (configuration?: Co
         /**
          * 
          * @summary convert Speech to Text
+         * @param {string} counselSessionId 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        convertSpeechToText(counselSessionId: string, options?: RawAxiosRequestConfig): AxiosPromise<SuccessRes> {
+            return localVarFp.convertSpeechToText(counselSessionId, options).then((request) => request(axios, basePath));
+        },
+        /**
+         * 
+         * @summary convert Speech to Text
          * @param {File} audio 
          * @param {ConvertSpeechToTextReq} body 
          * @param {*} [options] Override http request option.
          * @throws {RequiredError}
          */
-        convertSpeechToText(audio: File, body: ConvertSpeechToTextReq, options?: RawAxiosRequestConfig): AxiosPromise<SuccessRes> {
-            return localVarFp.convertSpeechToText(audio, body, options).then((request) => request(axios, basePath));
+        convertSpeechToText1(audio: File, body: ConvertSpeechToTextReq, options?: RawAxiosRequestConfig): AxiosPromise<SuccessRes> {
+            return localVarFp.convertSpeechToText1(audio, body, options).then((request) => request(axios, basePath));
         },
         /**
          * 
@@ -471,14 +532,26 @@ export class AICounselSummaryControllerApi extends BaseAPI {
     /**
      * 
      * @summary convert Speech to Text
+     * @param {string} counselSessionId 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof AICounselSummaryControllerApi
+     */
+    public convertSpeechToText(counselSessionId: string, options?: RawAxiosRequestConfig) {
+        return AICounselSummaryControllerApiFp(this.configuration).convertSpeechToText(counselSessionId, options).then((request) => request(this.axios, this.basePath));
+    }
+
+    /**
+     * 
+     * @summary convert Speech to Text
      * @param {File} audio 
      * @param {ConvertSpeechToTextReq} body 
      * @param {*} [options] Override http request option.
      * @throws {RequiredError}
      * @memberof AICounselSummaryControllerApi
      */
-    public convertSpeechToText(audio: File, body: ConvertSpeechToTextReq, options?: RawAxiosRequestConfig) {
-        return AICounselSummaryControllerApiFp(this.configuration).convertSpeechToText(audio, body, options).then((request) => request(this.axios, this.basePath));
+    public convertSpeechToText1(audio: File, body: ConvertSpeechToTextReq, options?: RawAxiosRequestConfig) {
+        return AICounselSummaryControllerApiFp(this.configuration).convertSpeechToText1(audio, body, options).then((request) => request(this.axios, this.basePath));
     }
 
     /**
